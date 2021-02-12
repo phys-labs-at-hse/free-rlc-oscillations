122aa0a314898335d858eccf777fda976cb1e1ca
nick <gruzinov15@yandex.ru>
2021-02-13 00:02:24 +0300

    Fix typos in report; infer R_{cr} from Θ(R) slope

--- qfactor_peaks.py
+++ qfactor_peaks.py
@@ -38,6 +38,7 @@ plt.savefig('qfactor_peaks.png')
 plt.close()
 
 slopes = np.array((0.370, 0.486, 0.614, 0.797, 0.914,))
+print(f'So, slopes for peaks are {slopes}')
 
 # Moving to the spiral method
 voltages_tuple = (
@@ -60,14 +61,16 @@ plt.savefig('qfactor_peaks_spiral.png')
 plt.close()
 
 slopes_spiral = np.array((0.343, 0.442, 0.589, 0.753,))
+print(f'So, slopes for spirals are {slopes_spiral}')
 
 # Compare the decrement coefs from different methods.
 # They should be proportional to R.
 plt.scatter(ress, slopes, label='По пикам')
 plt.scatter(ress[:-1], slopes_spiral, label='По спирали')
-slope, intercept = np.polyfit(ress[:-1], slopes_spiral, 1)
-plt.plot(ress, slope * ress + intercept, color='r')
-plt.plot(ress, 2 * np. pi / 440 * ress + intercept, color='r')
+merged_ress = np.array((*ress, *ress[:-1]))
+merged_slopes = np.array((*slopes, *slopes_spiral))
+slope, intercept = np.polyfit(merged_ress, merged_slopes, 1)
+plt.plot(merged_ress, slope * merged_ress + intercept, color='r')
 plt.grid()
 plt.legend()
 plt.xlabel('Сопротивление реостата', fontsize=14)
@@ -75,4 +78,4 @@ plt.ylabel('Декремент затухания', fontsize=14)
 plt.savefig('qfactor_prop_r.png')
 plt.close()
 
-print(f'Slope is {slope}, while it must be  2π / R_cr = 2π / 440 = {2 * np.pi / 440}')
+print(f'Critical resistance from the q-factor plot is {2 * np.pi / slope}')
